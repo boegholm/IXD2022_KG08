6c2bdec0acc92217858e35b057f925c9c3baa745
boegholm <github.com@glirc.dk>
2022-03-31 08:32:51 +0200

    Add project files.

---
+++ KG08/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace KG08
+{
+
+    internal class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            var p = new SampleData();
+            p.Parse();
+        }
+    }
+}
